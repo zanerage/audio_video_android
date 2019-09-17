34b4313c5f2258890c80b6285c2084dcc43b73b0
zanerage <marioantolovic0@gmail.com>
2019-09-17 18:23:02 +0200

    pozadine / play video / function / button stop pause music

--- app/src/main/java/com/mario_antolovic/audio_video_android/MainActivity.java
+++ app/src/main/java/com/mario_antolovic/audio_video_android/MainActivity.java
@@ -1,13 +1,43 @@
 package com.mario_antolovic.audio_video_android;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    private VideoView myVideoView;
+    private Button btnPlay;
+    private MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myVideoView = findViewById(R.id.myvideo);
+        btnPlay = findViewById(R.id.btn_play);
+
+        btnPlay.setOnClickListener(MainActivity.this);
+
+        mediaController = new MediaController(MainActivity.this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName()+ "/" + R.raw.butterfly);
+
+        myVideoView.setVideoURI(videoUri);
+
+        myVideoView.setMediaController(mediaController);
+        mediaController.setAnchorView(myVideoView);
+        myVideoView.start();
+
     }
 }
